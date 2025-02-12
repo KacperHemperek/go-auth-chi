7a7049eccab388368d85725da75a50c7b880fb46
Kacper Hemperek <kacper.hemperek@o2.pl>
2025-02-12 21:32:59 +0100

    feat(routes): mock register endpoint

---
+++ cmd/handlers.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// TODO: move to core
+type UserModel struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// TODO: move to core
+type CreateUserModel struct {
+	Email    string
+	Password string
+}
+
+// TODO: move to core
+func createUser(user CreateUserModel) (*UserModel, error) {
+	fmt.Println("User created")
+
+	return &UserModel{
+		ID:        1,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}, nil
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	type registerRequest struct {
+		Email           string `json:"email" validate:"required,min=3,max=255,email"`
+		Password        string `json:"password" validate:"required,min=8,max=30"`
+		ConfirmPassword string `json:"confirmPassword" validate:"required,min=8,max=30,eqfield=Password"`
+	}
+
+	req := &registerRequest{}
+	if err := readAndValidateJSON(w, r, req); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	user, err := createUser(CreateUserModel{Email: req.Email, Password: req.Password})
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	writeJSONResponse(w, http.StatusOK, map[string]any{"message": "User registered successfully", "user": user})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	r.Post("/auth/register", registerHandler)
+	fmt.Println("Server is running on port 2137")
+	http.ListenAndServe(":2137", r)
 }
