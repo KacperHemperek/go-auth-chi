41fb2773a5e611610f452063077842f415073859
Adrian Tworek <adrian.tworek.it@gmail.com>
2025-02-22 12:43:19 +0100

    feat: setup .env configuration and google oauth provider

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/sessions"
 	"github.com/kacperhemperek/go-auth-chi/internal/db"
 	"github.com/kacperhemperek/go-auth-chi/internal/mailer"
 	"github.com/kacperhemperek/go-auth-chi/internal/store"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"github.com/spf13/viper"
 )
 
+func init() {
+	viper.SetConfigFile(".env")
+	viper.SetDefault("BASE_URL", "http://localhost:8080")
+	viper.SetDefault("PORT", "8080")
+	viper.SetDefault("SESSION_SECRET", "change-me")
+	viper.SetDefault("DSN", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+		return
+	}
+}
+
 func main() {
+	gothic.GetProviderName = func(r *http.Request) (string, error) {
+		provider := chi.URLParam(r, "provider")
+		if provider == "" {
+			return "", fmt.Errorf("provider is required")
+		}
+		return provider, nil
+	}
+
+	sessionStore := sessions.NewCookieStore([]byte(viper.GetString("SESSION_SECRET")))
+	gothic.Store = sessionStore
+
+	goth.UseProviders(
+		google.New(viper.GetString("GOOGLE_CLIENT_ID"), viper.GetString("GOOGLE_CLIENT_SECRET"), fmt.Sprintf("%s/auth/google/callback", viper.GetString("BASE_URL")), "email", "profile"),
+	)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	db, err := db.NewPostgres("postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	db, err := db.NewPostgres(viper.GetString("DSN"))
 	if err != nil {
 		fmt.Println("Error connecting to the database")
 		fmt.Println(err)
@@ -28,6 +62,8 @@ func main() {
 		r.Post("/register", registerHandler(storage, mailer))
 		r.Post("/login", loginHandler(storage))
 		r.Put("/verify/{token}", verifyEmail(storage))
+		r.Get("/{provider}", gothic.BeginAuthHandler)
+		r.Get("/{provider}/callback", oauthCallbackHandler(storage))
 
 		// Protected routes
 		r.Group(func(r chi.Router) {
@@ -37,6 +73,7 @@ func main() {
 		})
 	})
 
-	fmt.Println("Server is running on port 2137")
-	http.ListenAndServe(":2137", r)
+	port := ":" + viper.GetString("PORT")
+	fmt.Printf("Server is running on port %s\n", port)
+	http.ListenAndServe(port, r)
 }
